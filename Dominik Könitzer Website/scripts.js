63b5b442fd4b308d707aeb055784588be3347080
Killua Zoldyck <82450286+Punds101@users.noreply.github.com>
2024-08-28 12:06:54 +0200

    Delete Dominik Könitzer Website

--- Dominik Könitzer Website/scripts.js
+++
@@ -1,6 +0,0 @@
-const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-mobileMenuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-});
